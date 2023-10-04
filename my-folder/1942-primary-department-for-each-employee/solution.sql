da41af6c91f3b1264f0433cf54846be81530a564
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2023-10-04 14:57:00 +0000

    [LeetCode Sync] Runtime - 1111 ms (32.06%), Memory - 0B (100.00%)

---
+++ my-folder/1942-primary-department-for-each-employee/solution.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+/*
+SELECT DISTINCT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y' OR employee_id IN (
+    SELECT employee_id
+    FROM Employee 
+    GROUP BY employee_id
+    HAVING COUNT(department_id) = 1
+);
+*/
+
+SELECT DISTINCT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(department_id) = 1
+
+UNION
+SELECT DISTINCT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
