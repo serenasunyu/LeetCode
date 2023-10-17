847bfa9fb9fdebfdb6d7d4e4cdfd8b98e26c506e
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2023-10-17 14:20:50 +0000

    [LeetCode Sync] Runtime - 1499 ms (28.48%), Memory - 0B (100.00%)

---
+++ my-folder/1462-list-the-products-ordered-in-a-period/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT p.product_name, SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o ON p.product_id = o.product_id
+WHERE MONTH(o.order_date) = 2 AND YEAR(o.order_date) = 2020
+GROUP BY p.product_name
+HAVING unit >= 100;
