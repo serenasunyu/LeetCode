587ab615e64b3e1e7f3df49353a095dd1b95b88b
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2023-10-30 16:03:56 +0000

    [LeetCode Sync] Runtime - 1127 ms (55.56%), Memory - 0B (100.00%)

--- my-folder/2087-confirmation-rate/solution.sql
+++ my-folder/2087-confirmation-rate/solution.sql
@@ -1,13 +1,6 @@
 # Write your MySQL query statement below
-
-SELECT 
-    s.user_id,
-    CASE 
-        WHEN c.time_stamp IS NULL THEN 0.00 
-        ELSE ROUND(SUM(action='confirmed')/count(action),2) 
-    END AS confirmation_rate
+SELECT s.user_id, ROUND(IFNULL(AVG(action = 'confirmed'), 0), 2) AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c
-ON c.user_id = s.user_id
+ON s.user_id = c.user_id
 GROUP BY s.user_id;
-
