a93e464e9f6d74335882a6be5bc96eb8812137b7
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2023-11-02 18:40:56 +0000

    [LeetCode Sync] Runtime - 1136 ms (21.43%), Memory - 0B (100.00%)

--- my-folder/2495-number-of-unique-subjects-taught-by-each-teacher/solution.sql
+++ my-folder/2495-number-of-unique-subjects-taught-by-each-teacher/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT DISTINCT teacher_id, COUNT(DISTINCT subject_id) AS cnt
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
 FROM Teacher
 GROUP BY teacher_id;
