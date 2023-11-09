4144ce8e8858858310c50be8866288792dbe16b4
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2023-11-09 15:50:31 +0000

    [LeetCode Sync] Runtime - 51 ms (5.83%), Memory - 16.2 MB (99.76%)

--- my-folder/0027-remove-element/solution.py
+++ my-folder/0027-remove-element/solution.py
@@ -1,8 +1,21 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        index = 0
+        # # Initialize a variable k to keep track of elements not equal to val
+        k = 0
+
+        # Iterate through the nums array
         for i in range(len(nums)):
+
+            # If the current element is not equal to val
             if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index
+
+                # Move the current element to the position of the first occurrence of val
+                nums[k] = nums[i]
+
+                # Increment k to maintain the count of elements not equal to val
+                k += 1
+        
+         # The first k elements in nums now contain elements not equal to val
+        # k represents the number of elements in nums which are not equal to val
+        return k
+
