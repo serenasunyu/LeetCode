d286e8ebd7141789ec4324a533b13d82a6d01714
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-04-26 04:26:45 +0000

    [LeetCode Sync] Runtime - 90 ms (26.46%), Memory - 62.1 MB (77.04%)

--- my-folder/0643-maximum-average-subarray-i/solution.js
+++ my-folder/0643-maximum-average-subarray-i/solution.js
@@ -4,18 +4,26 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    let maxSum = -Infinity;
-    let windowSum = 0;
+    // let maxSum = -Infinity;
+    // let windowSum = 0;
 
-    for (let i = 0; i < k; i++) {
-        windowSum += nums[i];
-    }
+    // for (let i = 0; i < k; i++) {
+    //     windowSum += nums[i];
+    // }
 
-    maxSum = windowSum;
+    // maxSum = windowSum;
+
+    // for(let i = k; i < nums.length; i++) {
+    //     windowSum += nums[i] - nums[i - k];
+    //     maxSum = Math.max(maxSum, windowSum)
+    // }
+
+    let sum = nums.slice(0, k).reduce((acc, num) => acc += num)
+    let maxSum = sum;
 
     for(let i = k; i < nums.length; i++) {
-        windowSum += nums[i] - nums[i - k];
-        maxSum = Math.max(maxSum, windowSum)
+        sum += nums[i] - nums[i - k];
+        maxSum = Math.max(maxSum, sum);
     }
     return maxSum/k;
 };
